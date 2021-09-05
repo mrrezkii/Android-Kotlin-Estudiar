ab4d8888930f851e99f848f8a9fe49d2650e0ee9
nando <muhammad.rezki.ananda@gmail.com>
2021-09-05 09:49:39 +0700

    Setup fragment binding

--- app/src/main/java/com/esdgabut/estudiar/data/view/fragment/BottomNav/ProfilFragment.kt
+++ app/src/main/java/com/esdgabut/estudiar/data/view/fragment/BottomNav/ProfilFragment.kt
@@ -1,21 +1,21 @@
-package com.esdgabut.estudiar.data.view.fragment.Onboarding
+package com.esdgabut.estudiar.data.view.fragment.BottomNav
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.esdgabut.estudiar.databinding.FragmentOnboarding1stBinding
+import com.esdgabut.estudiar.databinding.FragmentProfilBinding
 
-class Onboarding1stFragment : Fragment() {
+class ProfilFragment : Fragment() {
 
-    private lateinit var binding: FragmentOnboarding1stBinding
+    private lateinit var binding: FragmentProfilBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentOnboarding1stBinding.inflate(inflater, container, false)
+        binding = FragmentProfilBinding.inflate(inflater, container, false)
         return binding.root
     }
 
