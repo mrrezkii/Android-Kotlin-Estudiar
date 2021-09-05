a83d3bcb61476c9b556bf768e2ec390fd70e77da
nando <muhammad.rezki.ananda@gmail.com>
2021-09-05 20:38:01 +0700

    Slicing profil saya

--- app/src/main/java/com/esdgabut/estudiar/data/view/fragment/bottomnav/ProfilFragment.kt
+++ app/src/main/java/com/esdgabut/estudiar/data/view/fragment/bottomnav/ProfilFragment.kt
@@ -1,10 +1,14 @@
 package com.esdgabut.estudiar.data.view.fragment.bottomnav
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.esdgabut.estudiar.R
 import com.esdgabut.estudiar.databinding.FragmentProfilBinding
 
 class ProfilFragment : Fragment() {
@@ -17,6 +21,36 @@ class ProfilFragment : Fragment() {
     ): View? {
         binding = FragmentProfilBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvLayoutToolbar.tvToolbar.text = "Profil Saya"
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onStart() {
+        super.onStart()
+        val window = requireActivity().window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = requireActivity().resources.getColor(R.color.blue)
+
+        val view = window.decorView
+        view.systemUiVisibility = 0
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @Suppress("DEPRECATION")
+    override fun onPause() {
+        super.onPause()
+        val window = requireActivity().window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = requireActivity().resources.getColor(R.color.white)
+
+        val view = window.decorView
+        view.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
 }
