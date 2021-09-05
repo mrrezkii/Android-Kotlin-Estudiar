39d7dc529c1618ee5fbce7a94d016a7ff74fae95
nando <muhammad.rezki.ananda@gmail.com>
2021-09-05 11:31:33 +0700

    Add bottom nav, and configure beranda fragment

--- app/src/main/java/com/esdgabut/estudiar/data/view/activity/MainActivity.kt
+++ app/src/main/java/com/esdgabut/estudiar/data/view/activity/MainActivity.kt
@@ -1,137 +1,30 @@
 package com.esdgabut.estudiar.data.view.activity
 
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.esdgabut.estudiar.R
-import com.esdgabut.estudiar.data.view.adapter.TentorCardStackAdapter
 import com.esdgabut.estudiar.databinding.ActivityMainBinding
-import com.esdgabut.estudiar.utils.CardStackUtils
-import com.esdgabut.estudiar.utils.createTentors
-import com.yuyakaido.android.cardstackview.*
-import timber.log.Timber
 
-class MainActivity : AppCompatActivity(), CardStackListener {
+class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(
             layoutInflater
         )
     }
-    private val manager by lazy { CardStackLayoutManager(this, this) }
-    private val adapter by lazy { TentorCardStackAdapter(createTentors()) }
-    private val utils: CardStackUtils = CardStackUtils(manager, adapter)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setupNavigation()
-        setupCardStackView()
-        setupButton()
+        setupView()
     }
 
-    override fun onCardDragging(direction: Direction?, ratio: Float) {
-        Timber.d("onCardDragging: d = ${direction?.name}, r = $ratio")
+    private fun setupView() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.navView.setupWithNavController(navController)
     }
 
-    override fun onCardSwiped(direction: Direction?) {
-        Timber.d("onCardSwiped: p = ${manager.topPosition}, d = $direction")
-        if (manager.topPosition == adapter.itemCount - 5) {
-            utils.paginate()
-        }
-    }
 
-    override fun onCardRewound() {
-        Timber.d("onCardRewound: ${manager.topPosition}")
-    }
-
-    override fun onCardCanceled() {
-        Timber.d("onCardCanceled: ${manager.topPosition}")
-    }
-
-    override fun onCardAppeared(view: View?, position: Int) {
-        Timber.d("onCardAppeared: ($position)")
-    }
-
-    override fun onCardDisappeared(view: View?, position: Int) {
-        Timber.d("onCardDisappeared: ($position)")
-    }
-
-    private fun setupNavigation() {
-        val navigationView = binding.layoutCardTentor.navigationView
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.reload -> utils.reload()
-                R.id.add_spot_to_first -> utils.addFirst(1)
-                R.id.add_spot_to_last -> utils.addLast(1)
-                R.id.remove_spot_from_first -> utils.removeFirst(1)
-                R.id.remove_spot_from_last -> utils.removeLast(1)
-                R.id.replace_first_spot -> utils.replace()
-                R.id.swap_first_for_last -> utils.swap()
-            }
-            binding.layoutCardTentor.drawerLayout.closeDrawers()
-            true
-        }
-    }
-
-    private fun setupCardStackView() {
-        manager.setStackFrom(StackFrom.None)
-        manager.setVisibleCount(3)
-        manager.setTranslationInterval(8.0f)
-        manager.setScaleInterval(0.95f)
-        manager.setSwipeThreshold(0.3f)
-        manager.setMaxDegree(20.0f)
-        manager.setDirections(Direction.HORIZONTAL)
-        manager.setCanScrollHorizontal(true)
-        manager.setCanScrollVertical(true)
-        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
-        manager.setOverlayInterpolator(LinearInterpolator())
-
-        binding.layoutCardTentor.cardStackView.layoutManager = manager
-        binding.layoutCardTentor.cardStackView.adapter = adapter
-        binding.layoutCardTentor.cardStackView.itemAnimator.apply {
-            if (this is DefaultItemAnimator) {
-                supportsChangeAnimations = false
-            }
-        }
-    }
-
-    private fun setupButton() {
-        val skip = binding.layoutCardTentor.skipButton
-        skip.setOnClickListener {
-            val setting = SwipeAnimationSetting.Builder()
-                .setDirection(Direction.Left)
-                .setDuration(Duration.Normal.duration)
-                .setInterpolator(AccelerateInterpolator())
-                .build()
-            manager.setSwipeAnimationSetting(setting)
-            binding.layoutCardTentor.cardStackView.swipe()
-        }
-
-        val rewind = binding.layoutCardTentor.rewindButton
-        rewind.setOnClickListener {
-            val setting = RewindAnimationSetting.Builder()
-                .setDirection(Direction.Bottom)
-                .setDuration(Duration.Normal.duration)
-                .setInterpolator(DecelerateInterpolator())
-                .build()
-            manager.setRewindAnimationSetting(setting)
-            binding.layoutCardTentor.cardStackView.rewind()
-        }
-
-        val like = binding.layoutCardTentor.likeButton
-        like.setOnClickListener {
-            val setting = SwipeAnimationSetting.Builder()
-                .setDirection(Direction.Right)
-                .setDuration(Duration.Normal.duration)
-                .setInterpolator(AccelerateInterpolator())
-                .build()
-            manager.setSwipeAnimationSetting(setting)
-            binding.layoutCardTentor.cardStackView.swipe()
-        }
-    }
 }
--- app/src/main/java/com/esdgabut/estudiar/data/view/fragment/BottomNav/BerandaFragment.kt
+++ app/src/main/java/com/esdgabut/estudiar/data/view/fragment/BottomNav/BerandaFragment.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.esdgabut.estudiar.R
+import com.esdgabut.estudiar.data.view.adapter.TentorCardStackAdapter
 import com.esdgabut.estudiar.databinding.FragmentBerandaBinding
+import com.esdgabut.estudiar.utils.CardStackUtils
+import com.esdgabut.estudiar.utils.createTentors
+import com.yuyakaido.android.cardstackview.*
+import timber.log.Timber
 
-class BerandaFragment : Fragment() {
+class BerandaFragment : Fragment(), CardStackListener {
 
     private lateinit var binding: FragmentBerandaBinding
+    private lateinit var manager: CardStackLayoutManager
+    private lateinit var utils: CardStackUtils
+    private val adapter by lazy { TentorCardStackAdapter(createTentors()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +30,120 @@ class BerandaFragment : Fragment() {
     ): View? {
         binding = FragmentBerandaBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialized()
+        setupNavigation()
+        setupCardStackView()
+        setupButton()
+    }
+
+    private fun initialized() {
+        manager = CardStackLayoutManager(requireActivity().applicationContext, this)
+        utils = CardStackUtils(manager, adapter)
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {
+        Timber.d("onCardDragging: d = ${direction?.name}, r = $ratio")
+    }
+
+    override fun onCardSwiped(direction: Direction?) {
+        Timber.d("onCardSwiped: p = ${manager.topPosition}, d = $direction")
+        if (manager.topPosition == adapter.itemCount - 5) {
+            utils.paginate()
+        }
+    }
+
+    override fun onCardRewound() {
+        Timber.d("onCardRewound: ${manager.topPosition}")
+    }
+
+    override fun onCardCanceled() {
+        Timber.d("onCardCanceled: ${manager.topPosition}")
+    }
+
+    override fun onCardAppeared(view: View?, position: Int) {
+        Timber.d("onCardAppeared: ($position)")
+    }
+
+    override fun onCardDisappeared(view: View?, position: Int) {
+        Timber.d("onCardDisappeared: ($position)")
+    }
+
+    private fun setupNavigation() {
+        val navigationView = binding.layoutCardTentor.navigationView
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.reload -> utils.reload()
+                R.id.add_spot_to_first -> utils.addFirst(1)
+                R.id.add_spot_to_last -> utils.addLast(1)
+                R.id.remove_spot_from_first -> utils.removeFirst(1)
+                R.id.remove_spot_from_last -> utils.removeLast(1)
+                R.id.replace_first_spot -> utils.replace()
+                R.id.swap_first_for_last -> utils.swap()
+            }
+            binding.layoutCardTentor.drawerLayout.closeDrawers()
+            true
+        }
+    }
+
+    private fun setupCardStackView() {
+        manager.setStackFrom(StackFrom.None)
+        manager.setVisibleCount(3)
+        manager.setTranslationInterval(8.0f)
+        manager.setScaleInterval(0.95f)
+        manager.setSwipeThreshold(0.3f)
+        manager.setMaxDegree(20.0f)
+        manager.setDirections(Direction.HORIZONTAL)
+        manager.setCanScrollHorizontal(true)
+        manager.setCanScrollVertical(true)
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+        manager.setOverlayInterpolator(LinearInterpolator())
+
+        binding.layoutCardTentor.cardStackView.layoutManager = manager
+        binding.layoutCardTentor.cardStackView.adapter = adapter
+        binding.layoutCardTentor.cardStackView.itemAnimator.apply {
+            if (this is DefaultItemAnimator) {
+                supportsChangeAnimations = false
+            }
+        }
+    }
+
+    private fun setupButton() {
+        val skip = binding.layoutCardTentor.skipButton
+        skip.setOnClickListener {
+            val setting = SwipeAnimationSetting.Builder()
+                .setDirection(Direction.Left)
+                .setDuration(Duration.Normal.duration)
+                .setInterpolator(AccelerateInterpolator())
+                .build()
+            manager.setSwipeAnimationSetting(setting)
+            binding.layoutCardTentor.cardStackView.swipe()
+        }
+
+        val rewind = binding.layoutCardTentor.rewindButton
+        rewind.setOnClickListener {
+            val setting = RewindAnimationSetting.Builder()
+                .setDirection(Direction.Bottom)
+                .setDuration(Duration.Normal.duration)
+                .setInterpolator(DecelerateInterpolator())
+                .build()
+            manager.setRewindAnimationSetting(setting)
+            binding.layoutCardTentor.cardStackView.rewind()
+        }
+
+        val like = binding.layoutCardTentor.likeButton
+        like.setOnClickListener {
+            val setting = SwipeAnimationSetting.Builder()
+                .setDirection(Direction.Right)
+                .setDuration(Duration.Normal.duration)
+                .setInterpolator(AccelerateInterpolator())
+                .build()
+            manager.setSwipeAnimationSetting(setting)
+            binding.layoutCardTentor.cardStackView.swipe()
+        }
     }
 
 }
